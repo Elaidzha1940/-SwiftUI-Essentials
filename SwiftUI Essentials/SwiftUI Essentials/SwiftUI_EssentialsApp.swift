96a9e7cc471168ca5b1457ef26aa0f2edce7615a
Elaidzha1940 <eli080293@gmail.com>
2022-09-05 21:32:08 +0300

    Esse

---
+++ SwiftUI Essentials/SwiftUI Essentials/SwiftUI_EssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EssentialsApp.swift
+//  SwiftUI Essentials
+//
+//  Created by Elaidzha Shchukin on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
